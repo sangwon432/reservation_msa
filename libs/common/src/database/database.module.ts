3276fe14ede8b080fb3028e89f8d4fa5f5fc55a8
sangwon432 <swonl0622@gmail.com>
2024-08-11 06:21:18 -0700

    MSA settings

---
+++ libs/common/src/database/database.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { ModelDefinition, MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@app/common/config/config.module';
+import { ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+      }),
+    }),
+  ],
+})
+export class DatabaseModule {
+  static forFeature(models: ModelDefinition[]) {
+    return MongooseModule.forFeature(models);
+  }
+}
